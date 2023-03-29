441c54f14729a274210b2cdd283d19c6a288970d
inbashar <122408762+inbashar@users.noreply.github.com>
2023-03-29 13:54:08 -0400

    Add files via upload

---
+++ js/video.js
@@ -0,0 +1,59 @@
+var video;
+
+window.addEventListener("load", function() {
+	console.log("Good job opening the window")
+	video = document.querySelector("#player1")
+	video.autoplay = false
+	console.log("Autoplay is set to" + video.autoplay)
+	video.loop = false
+	console.log("Loop is set to " + video.loop)
+
+});
+
+document.querySelector("#play").addEventListener("click", function() {
+	video.play()
+	document.querySelector("#volume").innerHTML = video.volume * 010 + "%"
+ 	console.log("Play Video");
+});
+
+document.querySelector("#pause").addEventListener("click", function() {
+	video.pause()
+ 	console.log("Pause Video");
+});
+
+document.querySelector("#slower").addEventListener("click", function() {
+	video.playbackRate *= .95
+ 	console.log("Speed is" + video.playbackRate);
+});
+
+document.querySelector("#faster").addEventListener("click", function() {
+	video.playbackRate /= .95
+ 	console.log("Speed is" + video.playbackRate);
+});
+
+document.querySelector("#skip").addEventListener("click", function() {
+	video.currentTime += 15
+ 	console.log("Current time is" + video.currentTime);
+	if (video.currentTime >= video.duration)
+		video.currentTime = 0
+});
+
+document.querySelector("#mute").addEventListener("click", function() {
+	if (video.muted == true){
+		video.muted = false
+		console.log("Video unmuted")
+		this.innerHTML = "Mute"
+	}
+	else{
+		video.muted = true
+		console.log("Video muted")
+		this.innerHTML = "Unmute"
+	}
+});
+
+
+
+
+
+
+
